56d665dece80bb7fb3f3e14a46f05638562cee59
Weldon Sams <sams@ohsu.edu>
2017-11-29 14:47:28 -0800

    TD-95 added javadoc to login method

--- src/main/java/org/octri/authentication/controller/FormAuthenticationController.java
+++ src/main/java/org/octri/authentication/controller/FormAuthenticationController.java
@@ -3,6 +3,11 @@ package org.octri.authentication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller for handling form authentication.
+ * 
+ * @author sams
+ */
 @Controller
 public class FormAuthenticationController {
 
