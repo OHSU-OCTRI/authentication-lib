36bb2b66198eb67d46191f2c56c7515a2cf69862
Weldon <sams@ohsu.edu>
2019-08-28 11:12:50 -0700

    AUTHLIB-31 PR updates
    Bumped verison to 0.3.0-SNAPSHOT.
    
    Shortened error messages.

--- src/main/java/org/octri/authentication/server/security/password/Messages.java
+++ src/main/java/org/octri/authentication/server/security/password/Messages.java
@@ -13,27 +13,27 @@ public class Messages {
 
 	public static final String PASSWORD_INVALID = "The password does not meet all of the requirements.";
 
-	public static final String PASSWORD_TOO_SHORT = "Passwords must be at least 8 characters.";
+	public static final String PASSWORD_TOO_SHORT = "Password too short.";
 
-	public static final String PASSWORD_INSUFFICIENT_DIGIT = "Passwords must contain a number.";
+	public static final String PASSWORD_INSUFFICIENT_DIGIT = "Missing a number.";
 
-	public static final String PASSWORD_INSUFFICIENT_UPPERCASE = "Passwords must contain a capital letter.";
+	public static final String PASSWORD_INSUFFICIENT_UPPERCASE = "Missing a capital letter.";
 
-	public static final String PASSWORD_INSUFFICIENT_SPECIAL = "Passwords must contain a special character: space ! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ \\ ^ _ ` { | } ~";
+	public static final String PASSWORD_INSUFFICIENT_SPECIAL = "Missing a special character.";
 
 	/**
 	 * Note: INSUFFICIENT_CHARACTERISTICS is returned when either INSUFFICIENT_UPPERCASE or INSUFFICIENT_SPECIAL is
 	 * thrown.
 	 */
-	public static final String PASSWORD_INSUFFICIENT_CHARACTERISTICS = "Passwords must contain an additional capital letter OR a special character: space ! \" # $ % & ' ( ) * + , - . / : ; < = > ? @ \\ ^ _ ` { | } ~";
+	public static final String PASSWORD_INSUFFICIENT_CHARACTERISTICS = "Missing an additional capital letter OR a special character.";
 
 	public static final String MUST_NOT_USE_CURRENT_PASSWORD = "Must not use current password";
 
 	public static final String PASSWORDS_MUST_NOT_INCLUDE_USERNAME = "Passwords must not include username";
 
-	public static final String NEW_AND_CONFIRM_PASSWORDS_MISMATCH = "The value of 'New Password' does not match 'Confirm New Password'.";
+	public static final String NEW_AND_CONFIRM_PASSWORDS_MISMATCH = "New and Confirm passwords don't match.";
 
-	public static final String CURRENT_PASSWORD_INCORRECT = "Current password doesn't match existing password.";
+	public static final String CURRENT_PASSWORD_INCORRECT = "Current password incorrect.";
 
 	public static final String COULD_NOT_FIND_AN_EXISTING_USER = "Could not find an existing user.";
 
