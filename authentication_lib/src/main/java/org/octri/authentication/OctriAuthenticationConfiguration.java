20f75f5bea723891c2d5cb21d799839da82d6667
Heath Harrelson <harrelst@ohsu.edu>
2022-06-17 10:41:34 -0700

    AUTHLIB-105 Check Base URL Configuration
    Log an error if table-based authentication is enabled, but the
    application's base URL has not been configured.

--- authentication_lib/src/main/java/org/octri/authentication/OctriAuthenticationConfiguration.java
+++ authentication_lib/src/main/java/org/octri/authentication/OctriAuthenticationConfiguration.java
@@ -1,5 +1,9 @@
 package org.octri.authentication;
 
+import static org.octri.authentication.OctriAuthenticationProperties.DEFAULT_BASE_URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +15,19 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(OctriAuthenticationProperties.class)
 public class OctriAuthenticationConfiguration {
 
+	private static final Log log = LogFactory.getLog(OctriAuthenticationConfiguration.class);
+	private static final String BASE_URL_ERROR = "Table-based authentication is enabled but octri.authentication.base-url is "
+			+ DEFAULT_BASE_URL + ". Check the application's configuration.";
+
 	private OctriAuthenticationProperties authenticationProperties;
 
 	public OctriAuthenticationConfiguration(OctriAuthenticationProperties authenticationProperties) {
 		this.authenticationProperties = authenticationProperties;
+
+		if (DEFAULT_BASE_URL.equals(authenticationProperties.getBaseUrl())
+				&& authenticationProperties.getEnableTableBased()) {
+			log.error(BASE_URL_ERROR);
+		}
 	}
 
 	@Bean
--- authentication_lib/src/main/java/org/octri/authentication/OctriAuthenticationProperties.java
+++ authentication_lib/src/main/java/org/octri/authentication/OctriAuthenticationProperties.java
@@ -9,6 +9,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class OctriAuthenticationProperties {
 
 	/**
+	 * The default value of "octri.authentication.base-url".
+	 */
+	public static final String DEFAULT_BASE_URL = "http://localhost:8080";
+
+	/**
 	 * Whether LDAP authentication should be enabled.
 	 */
 	private Boolean enableLdap = false;
@@ -17,6 +22,23 @@ public class OctriAuthenticationProperties {
 	 * Whether table-based authentication should be enabled.
 	 */
 	private Boolean enableTableBased = false;
+
+	/**
+	 * The base URL of the application without a context path. Used to construct links sent in emails. Defaults to
+	 * the value of @link{OctriAuthenticationProperties.DEFAULT_BASE_URL}.
+	 */
+	private String baseUrl = DEFAULT_BASE_URL;
+
+	/**
+	 * Number of failed login attempts allowed before accounts are locked.
+	 */
+	private Integer maxLoginAttempts = 7;
+
+	/**
+	 * Length of time (in days) that credentials are valid. After this period has elapsed, users will be required
+	 * to change their password.
+	 */
+	private Integer credentialsExpirationPeriod = 180;
 
 	public Boolean getEnableLdap() {
 		return enableLdap;
@@ -34,10 +56,35 @@ public class OctriAuthenticationProperties {
 		this.enableTableBased = enableTableBased;
 	}
 
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	public Integer getMaxLoginAttempts() {
+		return maxLoginAttempts;
+	}
+
+	public void setMaxLoginAttempts(Integer maxLoginAttempts) {
+		this.maxLoginAttempts = maxLoginAttempts;
+	}
+
+	public Integer getCredentialsExpirationPeriod() {
+		return credentialsExpirationPeriod;
+	}
+
+	public void setCredentialsExpirationPeriod(Integer credentialsExpirationPeriod) {
+		this.credentialsExpirationPeriod = credentialsExpirationPeriod;
+	}
+
 	@Override
 	public String toString() {
-		return "OctriAuthenticationProperties [enableLdap=" + enableLdap + ", enableTableBased=" + enableTableBased
-				+ "]";
+		return "OctriAuthenticationProperties [baseUrl=" + baseUrl + ", credentialsExpirationPeriod="
+				+ credentialsExpirationPeriod + ", enableLdap=" + enableLdap + ", enableTableBased=" + enableTableBased
+				+ ", maxLoginAttempts=" + maxLoginAttempts + "]";
 	}
 
 }
