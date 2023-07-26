beba46fd6141351ba7d1565f3087d2f02c83ab7f
Heath Harrelson <harrelst@ohsu.edu>
2023-07-26 11:50:23 -0700

    AUTHLIB-123 Remove Bean from ErrorController
    Delete `@Bean` created by the `ErrorController` class. This was
    triggering a warning about a missing `@Configuration` annotation,
    and it does not seem to be needed with recent versions of Spring Boot.

--- authentication_lib/src/main/java/org/octri/authentication/server/controller/ErrorController.java
+++ authentication_lib/src/main/java/org/octri/authentication/server/controller/ErrorController.java
@@ -1,16 +1,8 @@
 package org.octri.authentication.server.controller;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,25 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ErrorController extends BasicErrorController {
-
-	/**
-	 * This Bean declaration provides the Spring Security Context to 4xx responses.
-	 * This is reported as fixed in Spring Boot 2.0.0:
-	 *
-	 * https://github.com/spring-projects/spring-boot/issues/1048
-	 *
-	 * @param springSecurityFilterChain
-	 * @return
-	 */
-	@Bean
-	public FilterRegistrationBean<Filter> getSpringSecurityFilterChainBindedToError(
-			@Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
-
-		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
-		registration.setFilter(springSecurityFilterChain);
-		registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
-		return registration;
-	}
 
 	public ErrorController(ErrorAttributes errorAttributes) {
 		super(errorAttributes, new ErrorProperties());
