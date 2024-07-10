7656dfe42ed6a28354113173d7ee9faf990df57e
Heath Harrelson <harrelst@ohsu.edu>
2024-07-10 13:09:01 -0700

    AUTHLIB-134 Embiggen Login Attempt Username
    Increase the size of the `username` field of the `login_attempt`
    table.

--- setup/optional_migrations/noemail/V20240709104000__alter_user_enlarge_columns.sql
+++ setup/optional_migrations/noemail/V20240709104000__alter_user_enlarge_columns.sql
@@ -1,4 +1,7 @@
--- AUTHLIB-134: Increases the size of the username email fields
+-- AUTHLIB-134: Increases the size of the username and email fields
 ALTER TABLE `user`
 	MODIFY `username` varchar(320) NOT NULL,
 	MODIFY `email` varchar(320);
+
+ALTER TABLE `login_attempt`
+	MODIFY `username` text NOT NULL;
