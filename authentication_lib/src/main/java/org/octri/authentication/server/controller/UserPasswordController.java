95b33b14adfca00a03affc878dbffc6c7c5b57c3
Heath Harrelson <harrelst@ohsu.edu>
2024-07-24 10:47:50 -0700

    AUTHLIB-119 Fix Extra Space in Flash Message

--- authentication_lib/src/main/java/org/octri/authentication/server/controller/UserPasswordController.java
+++ authentication_lib/src/main/java/org/octri/authentication/server/controller/UserPasswordController.java
@@ -39,7 +39,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @Scope("session")
 public class UserPasswordController {
 
-	public static final String EMAIL_SENT_CONFIRMATION_TEMPLATE = "An email was sent to your address containing instructions to change your password. This request will expire %s "
+	public static final String EMAIL_SENT_CONFIRMATION_TEMPLATE = "An email was sent to your address containing instructions to change your password. This request will expire %s"
 			+ ". If you do not receive an email in a few minutes please try again or contact your system administrator.";
 
 	public static final String GENERIC_ERROR_MESSAGE = "There was an unexpected error processing your request. "
